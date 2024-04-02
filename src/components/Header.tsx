01a43aa82a806f1f81dd4b9d3fa15f8169b8a713
Alex Dalene <aledal51469@stud.noroff.no>
2024-04-02 22:14:41 +0200

    Adds dropdown to toggle theme

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,8 @@
+import { useTheme } from './theme-provider';
+
 import { Link, useLocation } from 'react-router-dom';
 import { ReactNode } from 'react';
+
 import {
   NavigationMenu,
   NavigationMenuItem,
@@ -7,6 +10,14 @@ import {
   NavigationMenuList,
   navigationMenuTriggerStyle,
 } from '@/components/ui/navigation-menu';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { Button } from '@/components/ui/button';
+import { Moon, Sun } from 'lucide-react';
 
 type CustomLinkProps = {
   to: string;
@@ -27,8 +38,10 @@ const CustomLink = ({ to, children, ...props }: CustomLinkProps) => {
 };
 
 const Header = () => {
+  const { setTheme } = useTheme();
+
   return (
-    <NavigationMenu className='min-w-full justify-end pt-1'>
+    <NavigationMenu className="min-w-full justify-end px-2 pt-2">
       <NavigationMenuList>
         <NavigationMenuItem>
           <CustomLink to={'/'}>Home</CustomLink>
@@ -36,6 +49,26 @@ const Header = () => {
         <NavigationMenuItem>
           <CustomLink to={'/cart'}>Cart</CustomLink>
         </NavigationMenuItem>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" size="icon">
+              <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+              <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+              <span className="sr-only">Toggle theme</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem onClick={() => setTheme('light')}>
+              Light
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme('dark')}>
+              Dark
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme('system')}>
+              System
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </NavigationMenuList>
     </NavigationMenu>
   );
