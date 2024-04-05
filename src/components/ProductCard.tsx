69efd129c6e0c09a3424e4bde560716a467850ad
Alex Dalene <aledal51469@stud.noroff.no>
2024-04-05 07:24:12 +0200

    Makes products a component

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,92 @@
+import { Link } from 'react-router-dom';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Star } from 'lucide-react';
+
+interface Product {
+  id: number;
+  title: string;
+  description: string;
+  rating: number;
+  price: number;
+  discountedPrice: number;
+  image: {
+    url: string;
+    alt: string;
+  };
+}
+
+const ProductCard = (product: Product) => {
+  const calculateDiscount = (price: number, discountedPrice: number) => {
+    const difference = price - discountedPrice;
+    const percentage = (difference / price) * 100;
+
+    return Math.round(percentage);
+  };
+
+  return (
+    <Link to={`/product/${product.id}`} key={product.id}>
+      <Card className="group flex flex-col justify-between overflow-hidden">
+        <CardContent className="relative px-0">
+          {product.discountedPrice < product.price && (
+            <span className="absolute right-0 top-0 z-10 rounded-bl-lg bg-background px-2 py-1 text-sm font-bold text-primary">
+              -{calculateDiscount(product.price, product.discountedPrice)}%
+            </span>
+          )}
+          <img
+            src={product.image.url}
+            alt={product.image.alt}
+            className="h-48 w-full rounded-lg rounded-b-none object-cover transition-transform duration-300 ease-in-out group-hover:scale-105"
+          />
+        </CardContent>
+        <CardHeader>
+          <CardTitle className="line-clamp-1">{product.title}</CardTitle>
+          <CardDescription className="line-clamp-2">
+            {product.description}
+          </CardDescription>
+        </CardHeader>
+        <CardFooter className="relative flex-col items-start gap-6">
+          {product.rating === 0 ? (
+            <span className="invisible h-6 text-sm">No rating</span>
+          ) : (
+            <div className="flex">
+              {Array.from({ length: Math.round(product.rating) }).map(
+                (_, index) => (
+                  <span key={index}>
+                    <Star className="w-5 fill-muted-foreground stroke-none" />
+                  </span>
+                ),
+              )}
+              {Array.from({ length: 5 - Math.round(product.rating) }).map(
+                (_, index) => (
+                  <span key={index}>
+                    <Star className=" w-5 fill-muted stroke-none" />
+                  </span>
+                ),
+              )}
+            </div>
+          )}
+
+          {product.discountedPrice < product.price ? (
+            <div className="text-lg font-bold">
+              <p className="absolute top-8 text-xs font-normal text-muted-foreground">
+                BEFORE {product.price}
+              </p>
+              <p>NOW {product.discountedPrice}</p>
+            </div>
+          ) : (
+            <p className="text-lg font-bold">{product.price}</p>
+          )}
+        </CardFooter>
+      </Card>
+    </Link>
+  );
+};
+
+export default ProductCard;
--- src/routes/products/index.tsx
+++ src/routes/products/index.tsx
@@ -1,17 +1,16 @@
-import { Link, useLoaderData } from 'react-router-dom';
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
-import { Star } from 'lucide-react';
+import ProductCard from '@/components/ProductCard';
+import { useEffect } from 'react';
+
+import { useLoaderData } from 'react-router-dom';
+
+document.title = 'Home | E-commerce';
 
 const Products = () => {
   const { data } = useLoaderData() as { data: Array<Product> };
-  console.log(data);
+
+  useEffect(() => {
+    document.title = 'Home | E-commerce';
+  }, []);
 
   interface Product {
     id: number;
@@ -26,71 +25,10 @@ const Products = () => {
     };
   }
 
-  const calculateDiscount = (price: number, discountedPrice: number) => {
-    const difference = price - discountedPrice;
-    const percentage = (difference / price) * 100;
-
-    return Math.round(percentage);
-  };
-
   return (
-    <div className="grid grid-cols-3 gap-2">
+    <div className="grid grid-cols-2 gap-2 px-2 md:grid-cols-3 md:px-0">
       {data?.map((product: Product) => (
-        <Link to={`/product/${product.id}`} key={product.id}>
-          <Card className="group flex flex-col justify-between overflow-hidden">
-            <CardContent className="relative px-0">
-              {product.discountedPrice < product.price && (
-                <span className="absolute right-0 top-0 rounded-bl-lg bg-background px-2 py-1 text-sm font-bold text-primary">
-                  -{calculateDiscount(product.price, product.discountedPrice)}%
-                </span>
-              )}
-              <img
-                src={product.image.url}
-                alt={product.image.alt}
-                className="h-48 w-full rounded-lg rounded-b-none object-cover transition-transform duration-300 ease-in-out group-hover:scale-105"
-              />
-            </CardContent>
-            <CardHeader>
-              <CardTitle className="line-clamp-1">{product.title}</CardTitle>
-              <CardDescription className="line-clamp-2">
-                {product.description}
-              </CardDescription>
-            </CardHeader>
-            <CardFooter className="relative flex-col items-start gap-6">
-              {product.rating === 0 ? (
-                <span className="invisible text-sm">No rating</span>
-              ) : (
-                <div className="flex">
-                  {Array.from({ length: Math.round(product.rating) }).map(
-                    (_, index) => (
-                      <span key={index}>
-                        <Star className="w-5 fill-muted-foreground stroke-none" />
-                      </span>
-                    ),
-                  )}
-                  {Array.from({ length: 5 - Math.round(product.rating) }).map(
-                    (_, index) => (
-                      <span key={index}>
-                        <Star className=" w-5 fill-muted stroke-none" />
-                      </span>
-                    ),
-                  )}
-                </div>
-              )}
-
-              {product.discountedPrice < product.price ? (
-                <div className="text-lg font-bold">
-                  <p className="absolute top-8 text-xs font-normal text-muted-foreground">
-                    BEFORE {product.price}
-                  </p>
-                  <p>NOW {product.discountedPrice}</p>
-                </div>
-              ) : (
-                <p className="text-lg font-bold">{product.price}</p>
-              )}
-            </CardFooter>
-          </Card>
-        </Link>
+        <ProductCard {...product} key={product.id} />
       ))}
     </div>
   );
